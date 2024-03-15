46cec1cf895703398e721d06e99a0b8c94fcb103
Mikenziokah <mikenziokah@gmail.com>
2024-03-15 19:46:32 +0000

    addition

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,11 @@
-#!/user/bin/env python3
-""" functionf to add two numbers """
+#!/usr/bin/env python3
+"""
+functionf to add two numbers
+"""
 
 
 def add(a: float, b: float) -> float:
-    """ adds the two given parameters """
+    """ 
+    adds the two given parameters
+    """
     return a + b
