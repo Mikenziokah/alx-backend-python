e50ad2d22b193d8563342b555ac433a3d38ddb81
Mikenziokah <mikenziokah@gmail.com>
2024-03-15 19:53:57 +0000

    concanate

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""  function that concanates strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns the concanated strings"""
+    return str(str1) + str( str2)
